b8d092a9b4fbe13711ec7575d832de0fb839af78
Michal-Naron <michanaron@gmail.com>
2026-03-29 13:54:47 +0200

    refactor: create single endpoint responsible for rain, snowfall, wind, and other weather data

--- weather-service/weather_project/urls.py
+++ weather-service/weather_project/urls.py
@@ -1,9 +1,9 @@
-from weather.views import WeatherProcessor
+from weather.views import  WeatherProcessor
 
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/weather/', WeatherProcessor.as_view())
+    path('v1/weather/<str:weather>', WeatherProcessor.as_view())
 ]
