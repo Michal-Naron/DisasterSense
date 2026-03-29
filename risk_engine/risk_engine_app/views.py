e3232a337b16675b884847e61e85168d286edf8c
Michal-Naron <michanaron@gmail.com>
2026-03-29 23:14:08 +0200

    feat: implement initial risk calculation logic

--- risk_engine/risk_engine_app/views.py
+++ risk_engine/risk_engine_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+import requests
 
-# Create your views here.
+class RiskCalculator(APIView):
+    BASE_URL = "http://localhost:8000/v1/weather/"
+    
+    def get(self, requst):
+        weather_types = [
+        "snowfall","visibility","rain","temperature_2m",
+        "wind_speed_10m","cloud_cover","surface_pressure"
+        ]
+
+        data = {}
+
+        for weather in weather_types:
+            response = requests.get(f"{BASE_URL}{weather}")
+            response.raise_for_status()
+            data[weather] = response.json()[weather]  
