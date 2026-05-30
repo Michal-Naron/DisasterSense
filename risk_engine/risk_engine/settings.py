9906ad72bbbcc3aa33fe8110bc6303a938b6205a
Michal-Naron <michanaron@gmail.com>
2026-05-30 15:06:31 +0200

    feat: add complete risk engine implementation

--- risk_engine/risk_engine/settings.py
+++ risk_engine/risk_engine/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'risk_engine_app'
+    'risk_engine_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
