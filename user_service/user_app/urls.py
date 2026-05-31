e4ac9a78e5bfd9a1e8d860de2669fc31b8925527
Michal-Naron <michanaron@gmail.com>
2026-05-31 15:35:36 +0200

    feat: add API endpoint to update user data

--- user_service/user_app/urls.py
+++ user_service/user_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, PostFavoriteLocation
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('favorite-locations/', PostFavoriteLocation.as_view(), name='favorite-location-list')
+    path('edit-user', UserView.as_view(), name="edit-user")
 ]
--- user_service/user_app/views.py
+++ user_service/user_app/views.py
@@ -59,7 +59,7 @@ class LoginView(APIView):
         else:
             return Response({"detail": "Nieprawidłowe dane logowania"}, status=status.HTTP_401_UNAUTHORIZED)
 
-class PostFavoriteLocation(APIView):
+class FavoriteLocationView(APIView):
     def post(self, request):
         username = request.data.get("username")
         favorite_location = request.data.get("favorite_location")
@@ -96,3 +96,40 @@ class PostFavoriteLocation(APIView):
         locations_list = [loc.city_name for loc in locations]
         
         return Response({"favorite_locations": locations_list})
+
+
+class UserView(APIView):
+    def put(self, request):
+        username = request.data.get("username")
+        
+        if not username:
+            return Response({"error": "Nie podano nazwy użytkownika"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"error": "Użytkownik nie istnieje"}, status=status.HTTP_404_NOT_FOUND)
+        
+        first_name = request.data.get("first_name", user.first_name)
+        last_name = request.data.get("last_name", user.last_name)
+        email = request.data.get("email", user.email)
+        
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        
+        new_username = request.data.get("new_username")
+        if new_username and new_username != user.username:
+            if User.objects.filter(username=new_username).exists():
+                return Response({"error": "Ta nazwa użytkownika jest już zajęta"}, status=status.HTTP_400_BAD_REQUEST)
+            user.username = new_username
+
+        user.save()
+        
+        return Response({
+            "message": "Dane użytkownika zostały pomyślnie zaktualizowane",
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email
+        }, status=status.HTTP_200_OK)
