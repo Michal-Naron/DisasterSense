37fca2c23fe32c21c4c8ed1168d79310c60a63f4
Michal-Naron <michanaron@gmail.com>
2026-06-16 00:12:48 +0200

    feat: add LLM which describes weather

--- ocean-service/ocean/views.py
+++ ocean-service/ocean/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+
+def ai_message(request):
+    lat = request.GET.get("lat")
+    lon = request.GET.get("lon")
+
+    weather_rain = requests.get(
+        "http://weather-service:8000/v1/weather/rain",
+        params={"lat": lat, "lon": lon}
+    ).json()
+
+    weather_temperature = requests.get(
+        "http://weather-service:8000/v1/weather/temperature_2m",
+        params={"lat": lat, "lon": lon}
+    ).json()
+
+    weather_wind = requests.get(
+        "http://weather-service:8000/v1/weather/wind_speed_10m",
+        params={"lat": lat, "lon": lon}
+    ).json()
+
+    temperature = list(weather_temperature["temperature_2m"].values())[0]
+    rain = list(weather_rain["rain"].values())[0]
+    wind = list(weather_wind["wind_speed_10m"].values())[0]
+
+    prompt = f"""
+            You are a weather assistant.
+
+            Based on the provided weather data, generate exactly two short sentences.
+
+            Weather data:
+            - Temperature: {temperature}°C
+            - Rainfall: {rain} mm
+            - Wind speed: {wind} km/h
+
+            Requirements:
+            - First sentence: briefly describe the weather.
+            - Second sentence: recommend what to wear.
+            - Do not mention that you are an AI.
+            - Do not add greetings, jokes, warnings, or extra explanations.
+            - Maximum 25 words total.
+            - Return only the final message.
+        """
+
+    response = requests.post(
+            "http://ollama:11434/api/generate",
+            json={
+                "model": "qwen3:1.7b",
+                "prompt": prompt,
+                "stream": False
+            }
+        )
+
+    data = response.json()
+
+    return JsonResponse({
+            "message": data["response"]
+        })
--- user_service/user_app/views.py
+++ user_service/user_app/views.py
@@ -5,7 +5,7 @@ from .serializers import RegisterSerializer
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
-
+from .models import FavoriteLocation
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
